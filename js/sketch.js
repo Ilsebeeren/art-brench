606ad5ecfba1d7ede334bec0d491fd38af790e7c
Ilsebeeren <71121201+Ilsebeeren@users.noreply.github.com>
2020-09-15 15:41:41 +0200

    upload

---
+++ js/sketch.js
@@ -0,0 +1,23 @@
+function setup(){
+createCanvas(200, 200);
+background(100);
+console.log("Dit is de setup"); //of je gebruikt
+print("dit is de setup in printfunctie");
+
+
+rectMode(CENTER);
+ellipse(100, 100, 200, 200);
+fill(255, 0, 0);
+rect(100,100,20,20);
+
+fill(0,0,0);
+textAlign(CENTER, CENTER);
+text("dit is het midden", 100,100);
+
+
+}
+
+function draw(){
+
+
+}
